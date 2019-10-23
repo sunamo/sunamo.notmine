96f592c6206c31ebfc23c345252a300a94fbad5a
Radek Jančík <sunamocz@outlook.com>
2019-10-22 22:51:52 -0700

    From VPS 9/25/2019

---
+++ Collage.Engine/CreateCollageAsyncContext.cs
@@ -0,0 +1,18 @@
+﻿namespace Collage.Engine
+{
+    internal class CreateCollageAsyncContext
+    {
+        private readonly object sync = new object();
+        private bool isCancelling;
+
+        public bool IsCancelling
+        {
+            get { lock (this.sync) { return this.isCancelling; } }
+        }
+
+        public void Cancel()
+        {
+            lock (this.sync) { this.isCancelling = true; }
+        }
+    }
+}
